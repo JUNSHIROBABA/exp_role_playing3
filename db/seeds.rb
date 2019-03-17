34aee953c3924278b1a791e89e17ad40313e3e20
Kazushi Yamanishi <keironia187149@gmail.com>
2019-03-17 11:50:25 +0900

    initialize commit

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+User.create!(name: "yam", email: "a@a", password: "11111111", password_confirmation: "11111111")
+User.create!(name: "yamanishi", email: "a@b", password: "11111111", password_confirmation: "11111111")
+Group.create!(name: "group1")
+Group.create!(name: "group2")
+Member.create!(user_id: 1, group_id: 1)
+Member.create!(user_id: 2, group_id: 1)
+Member.create!(user_id: 1, group_id: 2)
+Member.create!(user_id: 2, group_id: 2)
